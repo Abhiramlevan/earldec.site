90897c238f30863a63c38688666b15a6904a2ee4
Abhiram levan <abhiramlevan2006@gmail.com>
2024-10-28 18:04:43 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateForm() {
     const username = document.getElementById("name").value.trim(); 
     const password = document.getElementById("password").value.trim(); 
 
-    if (username === "earldec" && password === "earldec@24") {
+    if (username === "earldec" && password === "54321") {
         return true; 
     } else {
         alert("Incorrect username or password. Please try again."); 
